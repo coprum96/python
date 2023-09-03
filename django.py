b505648f0a503cd3f4a3919ac3921cfd5d161593
coprum96 <medn@list.ru>
2023-09-03 10:05:55 +0300

    new

--- django.py
+++ django.py
@@ -11,7 +11,35 @@ print(6 % 4)
 #math functions
 print(round(3.9))
 print(abs(-20))
+print('hello'.capitalize())
+print('hello'.upper())
 
+# Define your current age
+my_age = 30  
 
-name = input("enter the name: Stas")
+# Calculate your age in 123879 years
+future_age = my_age + 123879
+greater = 3 < 4
+print(type(greater))
+my_age  = "43"
+
+my_name = "Rick"
+
+print(f"My name is " + my_name + ", and I am " + my_age + " years old.")
+"My name is Rick, and I am 43 years old."
+
+print(f"In 123879 years, I will be {future_age} years old.")
+
+name = "StAs"
 print(name.lower())
+
+my_hair_color = "black"
+print("My hair is", my_hair_color)
+"My hair is black"
+
+my_number = 5
+
+# Increment it
+my_number = my_number + 1
+
+print(my_number)
