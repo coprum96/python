350a2430d31a155778662abe0114f2ee454cd6a7
coprum96 <medn@list.ru>
2023-09-03 09:35:08 +0300

    new

---
+++ django.py
@@ -0,0 +1,17 @@
+print ("Stas")
+print("Hello world")
+print(type(2+2))
+print(type(2*2))
+print(type(2/2))
+print(type(50.556))
+print(2 ** 3)
+print(5 // 4)
+print(6 % 4)
+
+#math functions
+print(round(3.9))
+print(abs(-20))
+
+
+name = input("enter the name: Stas")
+print(name.lower())
